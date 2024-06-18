87ea6df9215c10efe937afabd82ca488a514ed19
raphadevflow <raphaelgomescontato7@gmail.com>
2024-06-17 21:22:06 -0300

    adicionado operador de comparação de maior ou igual que para organizar a forma de sorteio

--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
-function drawNumber(){
+function drawNumber() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
-    const result = Math.floor(Math.random () * (max - min +1)) + min;
 
-    alert(result)
+    if (max > min) {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+    } else {
+        alert("O valor minimo deve ser Menor que o valor Máximo")
+    }
+
 
 }
 
+/* Também posso fazer desta forma
+    if (min >= max) {
+        alert("O valor minimo deve ser Menor que o valor Máximo")
 
+
+    } else {
+         const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(result)
+    
+    }
+*/
 
 
 
